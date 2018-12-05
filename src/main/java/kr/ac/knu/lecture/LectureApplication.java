d9f7233348e7f6bc74c3bde8ab849f6f10acb59d
Jo SeongJun <jsjouo249@naver.com>
2018-12-05 19:21:55 +0900

    Deploy Test

--- src/main/java/kr/ac/knu/lecture/LectureApplication.java
+++ src/main/java/kr/ac/knu/lecture/LectureApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LectureApplication {
-
+  
 	public static void main(String[] args) {
 		SpringApplication.run(LectureApplication.class, args);
 	}
